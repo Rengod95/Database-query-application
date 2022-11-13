d65902ff315f7bc3c29b4c1a3de98ee81fac7819
Rengod95 <aorwn970@gmail.com>
2022-11-13 21:19:53 +0900

    fix deleter

--- data_search_app/client/src/sections/bottom/components/TupleDeleter.js
+++ data_search_app/client/src/sections/bottom/components/TupleDeleter.js
@@ -1,13 +1,20 @@
 import React, { useContext } from "react";
 import classes from "./styles/TupleDeleter.module.scss";
-import { DataStateContext } from "../../../store/DataContextProvider";
+import {DataDispatchContext, DataStateContext} from "../../../store/DataContextProvider";
 import APIHandler from "../../../api/APIHandler";
 
 const TupleDeleter = () => {
   const dataState = useContext(DataStateContext);
-
-  const deleteClickHandler = () => {
-    APIHandler.deleteTuple(dataState.checkedEmp);
+  const dispatcher = useContext(DataDispatchContext)
+  const deleteClickHandler = async () => {
+    await APIHandler.deleteTuple(dataState.checkedEmp).then(res=>{
+      APIHandler.getTuples(
+          dataState.totalCondition.searchCondition,
+          dataState.totalCondition.searchRange
+      ).then((res) => {
+        dispatcher.setTupleDataSet(res.data);
+      });
+    }).catch(e => console.log(e));
   };
 
   return (
